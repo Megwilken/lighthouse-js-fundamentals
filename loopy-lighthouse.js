ae6a7a71238c2e5bb66576902ee7fd5eeda0817c
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-11-03 19:33:49 -0600

    loopy lighthouse excecise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,12 @@
+for (let i = 100; i < 201; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (num % 3 === 0) {
+    console.log("Loopy");
+  } else if (num % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+console.log(i)
+  }
+}
+
